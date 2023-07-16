1b2ea25f7a36c0b918df49282f780aa85bc54cb8
Melih Hakan Pektas <melihhakanpektas@gmail.com>
2023-07-16 17:22:53 +0300

    merge from feyza-osman branch

--- lib/pages/home/homepage/advertisement/reviews/reviews_page.dart
+++ lib/pages/home/homepage/advertisement/reviews/reviews_page.dart
@@ -1,30 +1,91 @@
+import 'package:flutter/material.dart';
 import 'package:f151/components/custom_widgets.dart';
 import 'package:f151/models/advertisement_model.dart';
 import 'package:f151/models/person_model.dart';
-import 'package:flutter/material.dart';
 
 class ReviewsPage extends StatelessWidget {
   final AdvertisementModel adModel;
   final PersonModel adOwner;
-  const ReviewsPage({required this.adOwner, required this.adModel, super.key});
+  final TextEditingController commentController = TextEditingController();
 
+  ReviewsPage({required this.adOwner, required this.adModel, Key? key})
+      : super(key: key);
+
+  Future<void> addComment(String comment) async {
+    // Yorum ekleme işlemleri + yıldızla puanlama sistemi
+  }
+  Widget _buildStar(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          starRating = index + 1;
+        });
+      },
+      child: Icon(
+        Icons.star,
+        size: 40,
+        color: (index + 1) <= starRating ? Colors.amber : Colors.grey,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: CustomWidgets.appBar(title: const Text('Yorumlar')),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Burada hic yorum bulunamadi 🤷‍♂️'),
-            SizedBox(height: 40),
-            Text(
-                'Asagidaki bilgileri kullanarak bir sayfa tasarimi olusturabilirsiniz'),
-            Text(
-                'adModel ve adOwner field larini kullanarak bu sayfayi tasarlayabilirsiniz :)')
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Burada hiç yorum bulunamadı 🤷‍♂️'),
+          SizedBox(height: 40),
+          Text('Aşağıdaki bilgileri kullanarak bir sayfa tasarımı oluşturabilirsiniz'),
+          Text('adModel ve adOwner field\'larını kullanarak bu sayfayı tasarlayabilirsiniz :)'),
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) => AlertDialog(
+                  title: const Text('Yorum Ekle'),
+                  content: TextField(
+                    controller: commentController,
+                    decoration: const InputDecoration(
+                      hintText: 'Yorumunuzu buraya girin',
+                    ),
+                    maxLines: 3,
+                  ),
+                  actions: [
+                    TextButton(
+                      onPressed: () {
+                        final comment = commentController.text;
+                        if (comment.isNotEmpty) {
+                          addComment(comment);
+                        }
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('Ekle'),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        commentController.clear();
+                        Navigator.of(context).pop();
+                      },
+                      child: const Text('İptal'),
+                    ),
+                  ],
+                ),
+              );
+            },
+            child: const Text('Yorum Yap'),
+          ),
+        ],
       ),
     );
   }
+
+    
+
+
+
+  
+
 }
+
