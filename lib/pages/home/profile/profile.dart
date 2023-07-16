dcbf8a25b6fd7b1e091901156ccb85315cb4c343
seziyazgan <seziyazgann@gmail.com>
2023-07-16 15:26:51 +0300

    my ads label added

--- lib/pages/home/profile/profile.dart
+++ lib/pages/home/profile/profile.dart
@@ -5,13 +5,13 @@ import 'package:f151/bloc/chat_bloc.dart';
 import 'package:f151/components/custom_widgets.dart';
 import 'package:f151/pages/home/profile/create_advertisement/create_advertisement.dart';
 import 'package:f151/pages/home/profile/edit_profile/edit_profile_page.dart';
+import 'package:f151/pages/home/profile/my_ads_page.dart';
 import 'package:f151/pages/login/login_page.dart';
 import 'package:f151/services/auth/auth_helper.dart';
 import 'package:f151/services/onesignal/one_signal_api.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -34,147 +34,148 @@ class ProfileState extends State<Profile> {
           userId != null ? 'Profil' : 'Giriş Yap',
           key: const ValueKey('appNameHomepage'),
         ),
-        actions: [
-          IconButton(onPressed: () => null, icon: Icon(MdiIcons.dotsVertical))
-        ],
       ),
       body: userId == null
           ? const LoginPage()
-          : Center(
-              child: BlocBuilder<AppInfoBloc, AppInfoState>(
-                  builder: (context, state) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Column(
-                      children: [
-                        CircleAvatar(
-                          backgroundColor:
-                              const Color.fromRGBO(143, 110, 196, 1),
-                          radius: 80,
-                          foregroundImage: state.currentPerson.imageUrl == null
-                              ? null
-                              : NetworkImage(state.currentPerson.imageUrl!),
-                          child: state.currentPerson.imageUrl != null
-                              ? null
-                              : const Icon(
-                                  Icons.person,
-                                  size: 150,
-                                  color: Colors.white,
-                                ),
-                        ),
-                        const SizedBox(height: 16.0),
-                        Text(
-                          state.currentPerson.name,
-                          style: const TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 8.0),
-                        Text(
-                          'E-posta: ${state.currentPerson.email}',
-                          style: const TextStyle(fontSize: 16.0),
-                        ),
-                        const SizedBox(height: 8.0),
-                        Text(
-                          'Telefon: ${state.currentPerson.phone}',
-                          style: const TextStyle(fontSize: 16.0),
-                        ),
-                      ],
-                    ),
-                    Column(
-                      children: [
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => EditProfilePage()));
-                          },
-                          child: const Text(
-                            'Profili Düzenle',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            context.read<AppInfoBloc>().setPageIndex(1);
-                          },
-                          child: const Text(
-                            'Mesajlar',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            // Bildirimler sayfasına yönlendirme kodu buraya gelecek
-                          },
-                          child: const Text(
-                            'Bildirimler',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () => Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => const CreateAdvertisement(),
-                            ),
-                          ),
-                          child: const Text(
-                            'İlan Ver',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                        TextButton(
-                          onPressed: () {
-                            // Ayarlar sayfasına yönlendirme kodu buraya gelecek
-                          },
-                          child: const Text(
-                            'Ayarlar',
-                            style: TextStyle(
-                              fontSize: 16.0,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 30),
-                      child: ElevatedButton(
-                        onPressed: () async {
-                          final currentPerson =
-                              context.read<AppInfoBloc>().state.currentPerson;
-                          final newNotificaitonIds = [
-                            ...currentPerson.notificationIds
-                          ];
-                          final thisDevicePlayerId =
-                              await OneSignalApi.getPlayerId;
-                          newNotificaitonIds.remove(thisDevicePlayerId);
-                          final newPerson = currentPerson.copyWith(
-                              notificationIds: newNotificaitonIds);
-                          context
-                            ..read<AppInfoBloc>().clear()
-                            ..read<AdsBloc>().clear()
-                            ..read<ChatBloc>().clear();
-                          await FirebaseFirestore.instance
-                              .collection('users')
-                              .doc(FirebaseAuth.instance.currentUser!.uid)
-                              .set(newPerson.toMap())
-                              .then((value) => AuthHelper.signOut(context));
-                        },
-                        child: const Text('Çıkış Yap'),
+          : SingleChildScrollView(
+              child: Center(
+                child: BlocBuilder<AppInfoBloc, AppInfoState>(
+                    builder: (context, state) {
+                  return Column(
+                    children: [
+                      const SizedBox(
+                        height: 20,
                       ),
-                    ),
-                  ],
-                );
-              }),
+                      Column(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor:
+                                const Color.fromRGBO(143, 110, 196, 1),
+                            radius: 80,
+                            foregroundImage: state.currentPerson.imageUrl ==
+                                    null
+                                ? null
+                                : NetworkImage(state.currentPerson.imageUrl!),
+                            child: state.currentPerson.imageUrl != null
+                                ? null
+                                : const Icon(
+                                    Icons.person,
+                                    size: 150,
+                                    color: Colors.white,
+                                  ),
+                          ),
+                          const SizedBox(height: 16.0),
+                          Text(
+                            state.currentPerson.name,
+                            style: const TextStyle(
+                              fontSize: 24.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            'E-posta: ${state.currentPerson.email}',
+                            style: const TextStyle(fontSize: 16.0),
+                          ),
+                          const SizedBox(height: 8.0),
+                          Text(
+                            'Telefon: ${state.currentPerson.phone}',
+                            style: const TextStyle(fontSize: 16.0),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Column(
+                        children: [
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) =>
+                                      const EditProfilePage()));
+                            },
+                            child: const Text(
+                              'Profili Düzenle',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () {
+                              context.read<AppInfoBloc>().setPageIndex(1);
+                            },
+                            child: const Text(
+                              'Mesajlar',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) =>
+                                    const CreateAdvertisement(),
+                              ),
+                            ),
+                            child: const Text(
+                              'İlan Ver',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                          TextButton(
+                            onPressed: () => Navigator.of(context).push(
+                              MaterialPageRoute(
+                                builder: (context) => const MyAdsPage(),
+                              ),
+                            ),
+                            child: const Text(
+                              'İlanlarım',
+                              style: TextStyle(
+                                fontSize: 16.0,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 30),
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            final currentPerson =
+                                context.read<AppInfoBloc>().state.currentPerson;
+                            final newNotificaitonIds = [
+                              ...currentPerson.notificationIds
+                            ];
+                            final thisDevicePlayerId =
+                                await OneSignalApi.getPlayerId;
+                            newNotificaitonIds.remove(thisDevicePlayerId);
+                            final newPerson = currentPerson.copyWith(
+                                notificationIds: newNotificaitonIds);
+                            context
+                              ..read<AppInfoBloc>().clear()
+                              ..read<AdsBloc>().clear()
+                              ..read<ChatBloc>().clear();
+                            await FirebaseFirestore.instance
+                                .collection('users')
+                                .doc(FirebaseAuth.instance.currentUser!.uid)
+                                .set(newPerson.toMap())
+                                .then((value) => AuthHelper.signOut(context));
+                          },
+                          child: const Text('Çıkış Yap'),
+                        ),
+                      ),
+                    ],
+                  );
+                }),
+              ),
             ),
     );
   }
